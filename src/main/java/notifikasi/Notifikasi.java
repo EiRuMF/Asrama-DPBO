b88b8309f47de704434e9dd7660ee12f82c42dac
millychevalier <jamilurrahman839@gmail.com>
2026-06-23 18:18:47 +0700

    Class Notifikasi.java

--- src/main/java/notifikasi/Notifikasi.java
+++ src/main/java/notifikasi/Notifikasi.java
@@ -6,34 +6,86 @@ package notifikasi;
 
 import java.util.Date;
 
-/**
- *
- * @author IVAN
- */
 public class Notifikasi {
+
     private String idNotifikasi;
     private String pesan;
     private Date tanggal;
-    private boolean status;
+    private boolean sudahDibaca;
+    private String penerimaId;
+    private String tipe;
 
-    public Notifikasi(String idNotifikasi, String pesan, Date tanggal, boolean status) {
+    public Notifikasi(String idNotifikasi, String pesan, String penerimaId, String tipe) {
         this.idNotifikasi = idNotifikasi;
         this.pesan = pesan;
-        this.tanggal = tanggal;
-        this.status = status;
+        this.penerimaId = penerimaId;
+        this.tipe = tipe;
+        this.tanggal = new Date();
+        this.sudahDibaca = false;
     }
-    
-    public void kirimNotifikasi() {
-        
+
+    public String getIdNotifikasi() {
+        return idNotifikasi;
     }
-    
-    public void tandaiDibaca() {
-        
-    }
-    
+
     public String getPesan() {
-        
-        return null;
-        
+        return pesan;
+    }
+
+    public Date getTanggal() {
+        return tanggal;
+    }
+
+    public boolean isSudahDibaca() {
+        return sudahDibaca;
+    }
+
+    public String getPenerimaId() {
+        return penerimaId;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+
+    public void setPenerimaId(String penerimaId) {
+        this.penerimaId = penerimaId;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public void kirimNotifikasi() {
+
+        System.out.println("\n===== NOTIFIKASI =====");
+        System.out.println("Kepada : " + penerimaId);
+        System.out.println("Pesan  : " + pesan);
+        System.out.println("Tipe   : " + tipe);
+        System.out.println("======================");
+    }
+
+    public void tandaiDibaca() {
+        this.sudahDibaca = true;
+    }
+
+    @Override
+    public String toString() {
+
+        String status =
+                sudahDibaca ? "Sudah Dibaca" : "Belum Dibaca";
+
+        return "Notifikasi{" +
+                "idNotifikasi='" + idNotifikasi + '\'' +
+                ", pesan='" + pesan + '\'' +
+                ", tanggal=" + tanggal +
+                ", penerimaId='" + penerimaId + '\'' +
+                ", tipe='" + tipe + '\'' +
+                ", status='" + status + '\'' +
+                '}';
     }
 }
